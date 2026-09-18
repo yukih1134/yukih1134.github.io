2923af31dad82d9df80166ad058b2374b8ed7976
yukih1134 <yukih1134@gmail.com>
2026-09-18 14:55:54 +0800

    Refresh PWA cache v10 for playback audio session

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='miaomiao-desk-v9';
-const CORE=['./','./index.html','./styles.css?v=20260918-9','./app.js?v=20260918-9','./manifest.json','./icon.svg'];
+const CACHE='miaomiao-desk-v10';
+const CORE=['./','./index.html','./styles.css?v=20260918-10','./app.js?v=20260918-10','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
