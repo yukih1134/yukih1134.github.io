3251dfd363c835da9947b350ab7555dcf2b7d17c
yukih1134 <yukih1134@gmail.com>
2026-09-22 09:37:13 +0800

    Refresh PWA cache v19

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='miaomiao-desk-v18';
-const CORE=['./','./index.html','./styles.css?v=20260919-18','./app.js?v=20260919-18','./manifest.json','./icon.svg'];
+const CACHE='miaomiao-desk-v19';
+const CORE=['./','./index.html','./styles.css?v=20260922-19','./app.js?v=20260922-19','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 async function fetchGood(request){
